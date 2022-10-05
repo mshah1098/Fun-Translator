09670344a52dc84be4be2c47cdcd7bb062cc41ee
mtshah92 <mtshah92@gmail.com>
2022-10-05 09:08:53 +0530

    created yoda translator

---
+++ main.js
@@ -0,0 +1,28 @@
+var translatebtn = document.querySelector(".btn-translate");
+var txtArea = document.querySelector("textarea");
+var outputTxt = document.querySelector(".output-txt");
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+function translatedURL(input) {
+  return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler() {
+  console.log("error occured", error);
+  alert("something went wrong please try after some time");
+}
+
+function clickEventHander() {
+  var inputTxt = txtArea.value;
+
+  fetch(translatedURL(inputTxt))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedTxt = json.contents.translated;
+      outputTxt.innerText = translatedTxt;
+    })
+    .catch(errorHandler);
+}
+
+translatebtn.addEventListener("click", clickEventHander);
